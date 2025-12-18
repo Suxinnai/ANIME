0d5879f1a81c52ac0be1ebdd9a3805cf6d9e91c3
Suxinnai <Suxinnai@163.com>
2025-12-18 23:51:57 +0800

    chore: enable vercel adapter for api routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind({ applyBaseStyles: false }),
     mdx(),
     sitemap(),
     react(),
   ],
+
   prefetch: true,
+
+  // 如果你构建时报 sitemap 需要 site，就加上这一行：
+  // site: 'https://anime-2nzz.vercel.app',
 });
